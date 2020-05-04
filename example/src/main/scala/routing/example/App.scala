16d62b5e6a06a0fad4095ca62a44cf72c552a0ae
Matt Dziuban <mrdziuban@gmail.com>
2020-05-04 08:36:12 -0400

    Update example.

--- example/src/main/scala/routing/example/App.scala
+++ example/src/main/scala/routing/example/App.scala
@@ -3,10 +3,11 @@ package routing.example
 import cats.effect.{ExitCode, IO, IOApp}
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.syntax.kleisli._
+import scala.concurrent.ExecutionContext
 
 object App extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
-    BlazeServerBuilder[IO]
+    BlazeServerBuilder[IO](ExecutionContext.global)
       .bindHttp(8080, "localhost")
       .withHttpApp(Controller.actions.orNotFound)
       .serve
